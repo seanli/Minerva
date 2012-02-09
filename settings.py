952d0cd96b67472de808119b5333dc0d814aba35
Elesant <lishang106@gmail.com>
2012-02-09 17:56:15 -0600

    Merging.

--- settings.py
+++ settings.py
@@ -16,11 +16,11 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'minerva',                      # Or path to database file if using sqlite3.
-        'USER': 'minerva_admin',                      # Not used with sqlite3.
+        'NAME': 'lisean_minerva',                      # Or path to database file if using sqlite3.
+        'USER': 'lisean_minerva',                      # Not used with sqlite3.
         'PASSWORD': '1990106',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
