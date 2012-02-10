badc76c9a3e3e7b25649fc6fba04349bb1c8f52c
Sean Li <lishang106@gmail.com>
2012-02-09 17:10:52 -0800

    Pimped out dumpdata script.

--- dumpdata.py
+++ dumpdata.py
@@ -1,8 +1,23 @@
 import os
+import sys
+import re
 
-executable = raw_input("Please enter your python name (python/python2.7/etc): ")
-fixture_name = raw_input("Please enter your fixture name: ")
+executable = os.path.basename(sys.executable)
+script = sys.argv[0]
+directory = os.path.dirname(script)
+if directory != '':
+    directory += '/'
+valid = False
 
-os.system("%s manage.py dumpdata --indent=4 --natural --exclude=contenttypes --exclude=auth.Permission > fixtures/%s.json" % (executable, fixture_name))
+while not valid:
+    fixture_name = raw_input("Please enter your fixture name (alphabet and/or numbers only): ").strip()
+    alphnum = bool(re.match('[a-z0-9]+$', fixture_name, re.IGNORECASE))
+    if alphnum:
+        valid = True
 
-print "DONE!"
+result = os.system("%s %smanage.py dumpdata --indent=4 --natural --exclude=contenttypes --exclude=auth.Permission > %sfixtures/%s.json" % (executable, directory, directory, fixture_name))
+
+if result == 0:
+    print "Fixture: %s.json is generated." % fixture_name
+else:
+    print "Fixture generation has encountered error(s)."
