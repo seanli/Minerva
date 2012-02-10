ae02fc0b451a115ac50110555edd907bbbc7a7a1
Sean Li <lishang106@gmail.com>
2012-02-09 16:10:32 -0800

    Fixed dumpdata script.

--- dumpdata.py
+++ dumpdata.py
@@ -1,7 +1,8 @@
 import os
 
+executable = raw_input("Please enter your python name (python/python2.7/etc): ")
 fixture_name = raw_input("Please enter your fixture name: ")
 
-os.system("python manage.py dumpdata --indent=4 --natural --exclude contenttypes auth > fixtures/%s.json" % fixture_name)
+os.system("%s manage.py dumpdata --indent=4 --natural --exclude contenttypes auth > fixtures/%s.json" % (executable, fixture_name))
 
 print "DONE!"
