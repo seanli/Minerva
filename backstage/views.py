b9309b24f7e3538f240fa85ed3f55fd8c30f2d54
Sean Li <lishang106@gmail.com>
2012-03-01 00:53:56 -0800

    View permission decorators.

--- backstage/views.py
+++ backstage/views.py
@@ -2,9 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from Minerva.backstage.models import Ticket
+from Minerva.core.decorators import staff_required
 
 
 @login_required
+@staff_required
 def home(request):
     tickets = Ticket.objects.all()
     context = {
