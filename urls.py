041a5c57dae740ff3cb8dab644c4f14d811612bc
Elesant <lishang106@gmail.com>
2012-03-01 18:18:05 -0600

    Admin documentation.

--- urls.py
+++ urls.py
@@ -11,13 +11,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Minerva.views.home', name='home'),
     # url(r'^Minerva/', include('Minerva.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     url(r'^$', 'Minerva.core.views.bulletin', name='bulletin'),
     url(r'', include('Minerva.account.urls')),
     url(r'', include('Minerva.backstage.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^%s/' % settings.DAJAXICE_MEDIA_PREFIX, include('dajaxice.urls')),
 )
