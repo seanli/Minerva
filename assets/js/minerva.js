bc6c5ba6f607db49fd4d5ed78cb4417478f55599
Elesant <lishang106@gmail.com>
2012-03-18 03:28:06 -0700

    Confirm dialog improvements.

--- assets/js/minerva.js
+++ assets/js/minerva.js
@@ -5,36 +5,39 @@ function confirmModal(options) {
       message: 'The action requested may be irreversible.<br />Do you wish to continue?',
       yes: 'Yes',
       no: 'No',
-      callback : null
+      callback: null
   };
   var options = $.extend(defaults, options);
   
   var html = '<div class="modal fade" id="confirmModal"><div class="modal-header"><a class="close" data-dismiss="modal">&times;</a>' +
             '<h3>#Heading#</h3></div><div class="modal-body">' +
             '#Message#</div><div class="modal-footer">' +
-            '<a href="#" class="btn btn-primary" id="confirmYesBtn">#Yes#</a>' +
+            '<a href="#" class="btn btn-danger" id="confirmYesBtn">#Yes#</a>' +
             '<a href="#" class="btn" data-dismiss="modal">#No#</a></div></div>';
   html = html.replace('#Heading#', options.heading).replace('#Message#', options.message).replace('#Yes#', options.yes).replace('#No#', options.no);
   $('body').append(html);
   $('#confirmModal').modal('show');
   $('#confirmYesBtn').click(function() {
-      if(options.callback != null)
+      if (options.callback != null)
           options.callback();
       $('#confirmModal').modal('hide');
+  });
+  $('#confirmModal').on('hidden', function() {
+    $(this).remove();
   });
 };
 
 // Prevent form submission from pressing Enter key
 $(document).ready(function() {
   $(window).keypress(function(e) {
-    if(e.which == 13) {
+    if (e.which == 13) {
       var $targ = $(e.target);
-      if(!$targ.is("textarea") && !$targ.is(":button,:submit")) {
+      if (!$targ.is("textarea") && !$targ.is(":button,:submit")) {
         var focusNext = false;
         $(this).find(":input:visible:not([disabled],[readonly]), a").each(function() {
-          if(this === e.target) {
+          if (this === e.target) {
             focusNext = true;
-          } else if(focusNext) {
+          } else if (focusNext) {
             $(this).focus();
             return false;
           }
