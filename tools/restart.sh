0e58cba43d066f5d81e7f510f8b958e730c48634
Elesant <lishang106@gmail.com>
2012-03-24 00:14:37 -0500

    Django 1.4 production settings.

--- tools/restart.sh
+++ tools/restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python2.7 /home/lisean/webapps/minerva/Minerva/manage.py generate_static_dajaxice > /home/lisean/webapps/minerva/Minerva/assets/js/dajaxice.core.js
+python2.7 /home/lisean/webapps/minerva/Minerva/manage.py generate_static_dajaxice > /home/lisean/webapps/minerva/Minerva/schoolax/assets/js/dajaxice.core.js
 python2.7 /home/lisean/webapps/minerva/Minerva/manage.py collectstatic 
 /home/lisean/webapps/minerva/apache2/bin/./restart
 
