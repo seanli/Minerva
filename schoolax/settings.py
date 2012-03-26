0480910ee4f598db678e3e9d41585d85f4f7b36e
Elesant <lishang106@gmail.com>
2012-03-26 14:41:49 -0700

    QA settings.

--- schoolax/settings.py
+++ schoolax/settings.py
@@ -1,12 +1,18 @@
+import os
 import socket
 
 PROD_HOSTS = (
     'web330.webfaction.com',
 )
 
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
 HOST_NAME = socket.gethostname()
 
 if HOST_NAME in PROD_HOSTS:
-    from settings_prod import *
+    if 'minerva_qa' in PROJECT_ROOT:
+        from settings_qa import *
+    else:
+        from settings_prod import *
 else:
     from settings_dev import *
---
+++ schoolax/settings_qa.py
@@ -0,0 +1,23 @@
+from settings_base import *
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+DAJAXICE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lisean_minerva_qa',
+        'USER': 'lisean_minerva_qa',
+        'PASSWORD': '1990106',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+STATIC_ROOT = '/home/lisean/webapps/minerva_static/'
+STATIC_URL = 'http://schoolax.com/static/'
+MEDIA_ROOT = '/home/lisean/webapps/minerva_media/'
+MEDIA_URL = 'http://schoolax.com/media/'
+
+COMPRESS_ENABLED = True
