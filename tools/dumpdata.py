a060efb2ba26b649f52ee25b228092a50d886d0b
Elesant <lishang106@gmail.com>
2012-03-26 18:26:45 -0700

    Better dumpdata script.

--- tools/dumpdata.py
+++ tools/dumpdata.py
@@ -10,14 +10,61 @@ if directory != '':
 valid = False
 
 while not valid:
-    fixture_name = raw_input('Please enter your fixture name (alphabet and/or numbers only): ').strip()
-    alphnum = bool(re.match('[a-z0-9]+$', fixture_name, re.IGNORECASE))
+    fixture_collection = raw_input('Please enter your fixture collection name (alphabet and/or numbers only): ').strip()
+    alphnum = bool(re.match('[a-z0-9]+$', fixture_collection, re.IGNORECASE))
     if alphnum:
         valid = True
 
-result = os.system('%s ../%smanage.py dumpdata --indent=4 --natural --exclude=contenttypes --exclude=auth.Permission > ../schoolax/%sfixtures/%s.json' % (executable, directory, directory, fixture_name))
+pre_command = '%s ../%smanage.py dumpdata --indent=4 --natural ' % (executable, directory)
+fixture_collection_dir = '../schoolax/%sfixtures/%s' % (directory, fixture_collection)
+if not os.path.exists(fixture_collection_dir):
+    os.mkdir(fixture_collection_dir)
 
+json_name = 'all'
+result = os.system(pre_command + '--exclude=contenttypes --exclude=auth.Permission > %s/%s.json' % (fixture_collection_dir, json_name))
 if result == 0:
-    print 'Fixture: %s.json is generated.' % fixture_name
+    print 'Fixture: %s.json is generated.' % json_name
 else:
-    print 'Fixture generation has encountered error(s).'
+    print 'Fixture: %s.json generation failed.' % json_name
+
+json_name = 'account'
+result = os.system(pre_command + 'account > %s/%s.json' % (fixture_collection_dir, json_name))
+if result == 0:
+    print 'Fixture: %s.json is generated.' % json_name
+else:
+    print 'Fixture: %s.json generation failed.' % json_name
+
+json_name = 'backstage'
+result = os.system(pre_command + 'backstage > %s/%s.json' % (fixture_collection_dir, json_name))
+if result == 0:
+    print 'Fixture: %s.json is generated.' % json_name
+else:
+    print 'Fixture: %s.json generation failed.' % json_name
+    
+json_name = 'core'
+result = os.system(pre_command + 'core > %s/%s.json' % (fixture_collection_dir, json_name))
+if result == 0:
+    print 'Fixture: %s.json is generated.' % json_name
+else:
+    print 'Fixture: %s.json generation failed.' % json_name
+    
+json_name = 'course'
+result = os.system(pre_command + 'course > %s/%s.json' % (fixture_collection_dir, json_name))
+if result == 0:
+    print 'Fixture: %s.json is generated.' % json_name
+else:
+    print 'Fixture: %s.json generation failed.' % json_name
+
+json_name = 'ticket'
+result = os.system(pre_command + 'backstage.Ticket > %s/%s.json' % (fixture_collection_dir, json_name))
+if result == 0:
+    print 'Fixture: %s.json is generated.' % json_name
+else:
+    print 'Fixture: %s.json generation failed.' % json_name
+    
+json_name = 'wiki'
+result = os.system(pre_command + 'backstage.Wiki > %s/%s.json' % (fixture_collection_dir, json_name))
+if result == 0:
+    print 'Fixture: %s.json is generated.' % json_name
+else:
+    print 'Fixture: %s.json generation failed.' % json_name
