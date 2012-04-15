b36d84173ae8a9581b717dd29e41da8cbb1bab36
Elesant <lishang106@gmail.com>
2012-04-14 17:55:43 -0700

    Modification on settings code.

--- account/forms.py
+++ account/forms.py
@@ -63,20 +63,17 @@ class SignupForm(StandardForm):
     def clean_last_name(self):
         last_name = titlecase(self.cleaned_data['last_name'].strip())
         return last_name
+
+
 class SettingForm(StandardForm):
     password_old = forms.CharField(max_length=128, widget=forms.PasswordInput, label='Old Password')
     password = forms.CharField(max_length=128, widget=forms.PasswordInput, label='Password')
     password_conf = forms.CharField(max_length=128, widget=forms.PasswordInput, label='Confirm Password')
-    
-    def set_password(self):
-        self.request.user.set_password(self.cleaned_data['password'])
-        self.request.user.save()
-    
+
     def clean_password_old(self):
         if not self.request.user.check_password(self.cleaned_data['password_old']):
-            raise forms.ValidationError('Incorrect Password!')
-        
-    
+            raise forms.ValidationError('Incorrect password!')
+
     def clean_password_conf(self):
         password = self.cleaned_data['password']
         password_conf = self.cleaned_data['password_conf']
--- account/views.py
+++ account/views.py
@@ -3,8 +3,6 @@ from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse
-from django.contrib.auth.models import User
-from django.contrib.auth.hashers import check_password
 from account.forms import LoginForm, SignupForm, SettingForm
 from account.models import Profile
 from core.utilities import get_referrer, set_referrer
@@ -47,14 +45,18 @@ def signup(request):
     context = RequestContext(request)
     context['form'] = form
     return render_to_response('account/signup.html', context)
-def setting(request):
+
+
+def settings(request):
+    user = request.user
     if request.method == 'POST':
-        form = SettingForm(request.POST,request=request)
+        form = SettingForm(request.POST, request=request)
         if form.is_valid():
             data = form.cleaned_data
-            form.set_password()
+            user.set_password(data['password'])
+            user.save()
     else:
         form = SettingForm()
     context = RequestContext(request)
     context['form'] = form
-    return render_to_response('account/setting.html', context)
+    return render_to_response('account/settings.html', context)
