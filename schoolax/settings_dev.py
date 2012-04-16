5feadb05b37cbd43f2a5b9ef306bf8f94fb8c113
Elesant <lishang106@gmail.com>
2012-04-16 11:51:19 -0700

    Settings structure change.

--- schoolax/settings_dev.py
+++ schoolax/settings_dev.py
@@ -27,54 +27,48 @@ COMPRESS_ENABLED = False
 # Render CSS Using LESS (Slower)
 
 USE_LESS = False
-
-if USE_LESS:
-    TEMPLATE_VISIBLE_SETTINGS += (
-        'USE_LESS',
-    )
+TEMPLATE_VISIBLE_SETTINGS += (
+    'USE_LESS',
+)
 
 # Swap User Feature
 
 SWAP_USER = True
-
-if SWAP_USER:
-    TEMPLATE_VISIBLE_SETTINGS += (
-        'SWAP_USER',
-    )
+TEMPLATE_VISIBLE_SETTINGS += (
+    'SWAP_USER',
+)
 
 # Django Debug Toolbar
 
 ENABLE_TOOLBAR = False
 
-if ENABLE_TOOLBAR:
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
 
-    MIDDLEWARE_CLASSES += (
-        'debug_toolbar.middleware.DebugToolbarMiddleware',
-    )
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
 
-    INSTALLED_APPS += (
-        'debug_toolbar',
-    )
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.profiling.ProfilingDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.cache.CacheDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
 
-    DEBUG_TOOLBAR_PANELS = (
-        'debug_toolbar.panels.version.VersionDebugPanel',
-        'debug_toolbar.panels.timer.TimerDebugPanel',
-        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-        'debug_toolbar.panels.headers.HeaderDebugPanel',
-        'debug_toolbar.panels.profiling.ProfilingDebugPanel',
-        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-        'debug_toolbar.panels.sql.SQLDebugPanel',
-        'debug_toolbar.panels.template.TemplateDebugPanel',
-        'debug_toolbar.panels.cache.CacheDebugPanel',
-        'debug_toolbar.panels.signals.SignalDebugPanel',
-        'debug_toolbar.panels.logger.LoggingPanel',
-    )
+def toolbar_callback(request):
+    return ENABLE_TOOLBAR
 
-    def toolbar_callback(request):
-        return ENABLE_TOOLBAR
-
-    DEBUG_TOOLBAR_CONFIG = {
-        'INTERCEPT_REDIRECTS': True,
-        'SHOW_TOOLBAR_CALLBACK': toolbar_callback,
-        'ENABLE_STACKTRACES': True,
-    }
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': True,
+    'SHOW_TOOLBAR_CALLBACK': toolbar_callback,
+    'ENABLE_STACKTRACES': True,
+}
