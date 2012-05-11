ab608f7ec18df92424591acacae544818e81212e
Elesant <lishang106@gmail.com>
2012-05-11 09:06:56 -0700

    Production polishing.

--- bulletin/urls.py
+++ bulletin/urls.py
@@ -4,5 +4,4 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('bulletin.views',
     # Home Page
     url(r'^bulletin/', 'bulletin', name='bulletin'),
-    url(r'^$', 'bulletin', name='home'),
 )
--- homeroom/urls.py
+++ homeroom/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('homeroom.views',
     url(r'^homeroom/$', 'homeroom', name='homeroom'),
     url(r'^homeroom/class/(?P<section_id>\d+)/$', 'class_section'),
+    url(r'^$', 'homeroom', name='home'),
 )
