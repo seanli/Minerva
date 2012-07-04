72074d68821f29cb7f877a92db797920d47ef956
Elesant <lishang106@gmail.com>
2012-07-04 17:06:50 -0400

    Refactored CSS.

---
+++ schoolax/assets/js/vendor/minerva-widget.js
@@ -0,0 +1,95 @@
+// Sticky elements
+(function($) {
+  /**
+   * Add our plugin to the jQuery.fn object
+   */
+  $.fn.sticky = function(options) {
+    /**
+     * Define some default settings
+     */
+    var defaults = {
+      // #{EDIT-HERE}# Your default options go here ...
+    };
+    /**
+     * Merge the runtime options with the default settings
+     */
+    var options = $.extend({}, defaults, options);
+    /**
+     * Iterate through the collection of elements and
+     * return the object to preserve method chaining
+     */
+    return this.each(function(i) {
+      /**
+       * Wrap the current element in an instance of jQuery
+       */
+      var $this = $(this);
+      $this.waypoint(function(event, direction) {
+        if(direction === 'down') {
+          elem_width = $this.width();
+          $this.addClass('sticky');
+          $this.css('width', elem_width);
+        } else {
+          $this.removeClass('sticky');
+          $this.removeAttr('style');
+        }
+        event.stopPropagation();
+      }, {
+        onlyOnScroll : true,
+      });
+    });
+  };
+})(jQuery);
+
+// Auto-complate elements (extends JQuery UI Autocomplete)
+(function($) {
+  $.fn.auto_complete = function(options) {
+    var defaults = {
+      minLength : 1,
+      autoFocus : true,
+      delay : 200,
+      focus : function(event, ui) {
+        return false;
+      },
+      // source : 'url...'
+      // select : function(event, ui) { ... }
+      // search : function(event, ui) { ... }
+      // render : function(ul, item) { ... }
+      render : null,
+    };
+    var options = $.extend({}, defaults, options);
+    return this.each(function(i) {
+      var $this = $(this);
+      if(options.render != null) {
+        $this.autocomplete(options).data('autocomplete')._renderItem = options.render;
+      } else {
+        $this.autocomplete(options);
+      }
+    });
+  };
+})(jQuery);
+
+// AJAX forms
+(function($) {
+  $.fn.ajax_form = function(options) {
+    var defaults = {
+      preSubmit : function() {},
+      trigger : null, // Button element which triggers submit
+      appFunction : '', // The Dajax view called in the format: app.ajax.function
+      // Callback function is separately declared and called using Dajax mechanism
+    };
+    var options = $.extend({}, defaults, options);
+    return this.each(function(i) {
+      var $this = $(this);
+      var $trigger = options.trigger;
+      $trigger.click(function() {
+        // Calls the preSubmit function
+        options.preSubmit();
+        var form_data = $this.serializeObject(true);
+        var appFunction = options.appFunction.split('.');
+        Dajaxice[appFunction[0]][appFunction[1]](Dajax.process, {
+          'form_data' : form_data
+        });
+      });
+    });
+  };
+})(jQuery);
---
+++ schoolax/assets/js/vendor/minerva.js
@@ -0,0 +1,142 @@
+// Raise a confirmation modal dialog
+function confirm_modal(options) {
+  var defaults = {
+    heading : 'Please Confirm Action',
+    message : 'The action requested may be irreversible.<br />Do you wish to continue?',
+    yes : 'Yes',
+    no : 'No',
+    callback : null
+  };
+  var options = $.extend(defaults, options);
+
+  var html = '<div class="modal fade" id="confirm-modal"><div class="modal-header"><a class="close" data-dismiss="modal">&times;</a>' + 
+    '<h3>#Heading#</h3></div><div class="modal-body">' + 
+    '#Message#</div><div class="modal-footer">' + 
+    '<a href="#" class="btn btn-danger" id="confirm-yes">#Yes#</a>' + 
+    '<a href="#" class="btn" data-dismiss="modal">#No#</a></div></div>';
+  html = html.replace('#Heading#', options.heading).replace('#Message#', options.message).replace('#Yes#', options.yes).replace('#No#', options.no);
+  $('body').append(html);
+  $('#confirm-modal').modal('show');
+  $('#confirm-yes').click(function() {
+    if(options.callback != null)
+      options.callback();
+    $('#confirm-modal').modal('hide');
+  });
+  $('#confirm-modal').on('hidden', function() {
+    $(this).remove();
+  });
+};
+
+// Raise a message modal dialog
+function message_modal(options) {
+  var defaults = {
+    heading : 'Message Heading',
+    message : 'This is the actual message.',
+    ok : 'OK',
+  };
+  var options = $.extend(defaults, options);
+
+  var html = '<div class="modal fade" id="message-modal"><div class="modal-header"><a class="close" data-dismiss="modal">&times;</a>' + 
+    '<h3>#Heading#</h3></div><div class="modal-body">' + 
+    '#Message#</div><div class="modal-footer">' + 
+    '<a href="#" class="btn btn-primary" data-dismiss="modal">#OK#</a></div></div>';
+  html = html.replace('#Heading#', options.heading).replace('#Message#', options.message).replace('#OK#', options.ok);
+  $('body').append(html);
+  $('#message-modal').modal('show');
+  $('#message-modal').on('hidden', function() {
+    $(this).remove();
+  });
+};
+
+// Escapes regex characters
+RegExp.escape = function(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
+
+// Javascript error logging
+  var prevOnError = window.onerror;
+  window.onerror = function(errorMsg, url, lineNumber) {
+    if(typeof(jQuery) != 'undefined') {
+      jQuery.ajax({ url: '/onerror/',
+        type: 'POST',
+        data: {
+          message: errorMsg,
+          line_number: lineNumber,
+          url: url
+        }
+      });
+    }
+    if(prevOnError) {
+      return prevOnError(errorMsg, url, lineNumber);
+    }
+    return false;
+  }
+
+$(document).ready(function() {
+  // Prevent form submission from pressing Enter key
+  /*$(window).keypress(function(e) {
+    if(e.which == 13) {
+      var $targ = $(e.target);
+      if(!$targ.is("textarea") && !$targ.is(":button,:submit")) {
+        var focusNext = false;
+        $(this).find(":input:visible:not([disabled],[readonly]), a").each(function() {
+          if(this === e.target) {
+            focusNext = true;
+          } else if(focusNext) {
+            $(this).focus();
+            return false;
+          }
+        });
+        return false;
+      }
+    }
+  });*/
+  // Setup Prettify
+  prettyPrint();
+  // Setup Popovers
+  $("a[rel=popover]")
+    .popover()
+    .click(function(e) {
+      e.preventDefault()
+    })
+  // Sticky element demo
+  $('.tooltip-demo').sticky();
+  // Tooltip demo
+  $('.tooltip-demo').tooltip({
+    selector : "a[rel=tooltip]",
+  });
+});
+
+// Cross-platform placeholder
+/* <![CDATA[ */
+$(function() {
+  var input = document.createElement("input");
+    if(('placeholder' in input)==false) { 
+    $('[placeholder]').focus(function() {
+      var i = $(this);
+      if(i.val() == i.attr('placeholder')) {
+        i.val('').removeClass('placeholder');
+        if(i.hasClass('password')) {
+          i.removeClass('password');
+          this.type='password';
+        }     
+      }
+    }).blur(function() {
+      var i = $(this);  
+      if(i.val() == '' || i.val() == i.attr('placeholder')) {
+        if(this.type=='password') {
+          i.addClass('password');
+          this.type='text';
+        }
+        i.addClass('placeholder').val(i.attr('placeholder'));
+      }
+    }).blur().parents('form').submit(function() {
+      $(this).find('[placeholder]').each(function() {
+        var i = $(this);
+        if(i.val() == i.attr('placeholder'))
+          i.val('');
+      })
+    });
+  }
+});
+/* ]]> */
