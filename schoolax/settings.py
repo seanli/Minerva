045162c5b33ca117656f163d27605cdea6241576
Elesant <lishang106@gmail.com>
2012-07-06 12:31:49 -0400

    Heroku check.

--- schoolax/settings.py
+++ schoolax/settings.py
@@ -1,10 +1,12 @@
 import os
 import socket
+import re
 
 PROD_HOSTS = (
     'web330.webfaction.com',
-    'f573d464-26db-4433-9ede-00a506f3141d',
 )
+
+HEROKU_HOST_REGEX = re.compile('(.)(.)(.)(.)(.)(.)(.)(.)(-)(.)(.)(.)(.)(-)(.)(.)(.)(.)(-)(.)(.)(.)(.)(.)(.)(.)(.)(.)(.)(.)(.)')
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
@@ -16,7 +18,11 @@ if HOST_NAME in PROD_HOSTS:
     else:
         from settings_prod import *
 else:
-    from settings_dev import *
+    heroku_match = HEROKU_HOST_REGEX.search(HOST_NAME)
+    if heroku_match:
+        from settings_prod import *
+    else:
+        from settings_dev import *
 
 # You can add a settings_extra.py file for additional personal configurations
 if os.path.isfile(os.path.join(PROJECT_ROOT, 'settings_extra.py')):
